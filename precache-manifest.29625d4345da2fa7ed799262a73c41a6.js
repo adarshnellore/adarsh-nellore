bf77d9457c3cc3c1994534dfb849579659db3362
adarshnellore <AdiNellore@gmail.com>
2020-10-29 18:03:35 -0700

    Updates

---
+++ precache-manifest.29625d4345da2fa7ed799262a73c41a6.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ab2ea9e72470ab298ec",
+    "url": "/adarsh-nellore/static/css/main.f754b523.chunk.css"
+  },
+  {
+    "revision": "6ab2ea9e72470ab298ec",
+    "url": "/adarsh-nellore/static/js/main.6ab2ea9e.chunk.js"
+  },
+  {
+    "revision": "747a42a7c3ee4a554e29",
+    "url": "/adarsh-nellore/static/js/runtime~main.747a42a7.js"
+  },
+  {
+    "revision": "a946c43350bd10b7b5ca",
+    "url": "/adarsh-nellore/static/css/2.a100651a.chunk.css"
+  },
+  {
+    "revision": "a946c43350bd10b7b5ca",
+    "url": "/adarsh-nellore/static/js/2.a946c433.chunk.js"
+  },
+  {
+    "revision": "59ac3aa13e88be39ee453a55b59365ea",
+    "url": "/adarsh-nellore/static/media/AI_Image2_2.59ac3aa1.jpg"
+  },
+  {
+    "revision": "30e0ebb0e9a53792686642a1c354a2a2",
+    "url": "/adarsh-nellore/static/media/sitting.30e0ebb0.jpg"
+  },
+  {
+    "revision": "e43320914ea68c52f896beff133c7f5b",
+    "url": "/adarsh-nellore/static/media/centerpoint.e4332091.mov"
+  },
+  {
+    "revision": "6d0d2c10bf40fbb442b6f7bd4b903200",
+    "url": "/adarsh-nellore/static/media/chairdiagram2.6d0d2c10.jpg"
+  },
+  {
+    "revision": "8ca2f34fa3be40f40c83bc293d7e657e",
+    "url": "/adarsh-nellore/static/media/centerpointwhole.8ca2f34f.jpg"
+  },
+  {
+    "revision": "598e7e3f72e1182ad2606da5c241dd16",
+    "url": "/adarsh-nellore/static/media/Space1.598e7e3f.jpg"
+  },
+  {
+    "revision": "550d6331815d1141e32126b7c70404f8",
+    "url": "/adarsh-nellore/static/media/catheter.550d6331.jpg"
+  },
+  {
+    "revision": "a1d72c24f394e3656c8d08487c4fe2b1",
+    "url": "/adarsh-nellore/static/media/HeartValves3.a1d72c24.jpg"
+  },
+  {
+    "revision": "e02d9c6cfe560d04e29986289d6ea196",
+    "url": "/adarsh-nellore/static/media/Valve.e02d9c6c.gif"
+  },
+  {
+    "revision": "1a4377558ecee835798d201487ffd3e2",
+    "url": "/adarsh-nellore/static/media/edwards.1a437755.mp4"
+  },
+  {
+    "revision": "ad528475d7a0266634ebbe1bc0b2fedb",
+    "url": "/adarsh-nellore/static/media/spatialIntro.ad528475.jpg"
+  },
+  {
+    "revision": "a63ac83d108e56a501b9c445bc77556b",
+    "url": "/adarsh-nellore/static/media/reverb.a63ac83d.jpg"
+  },
+  {
+    "revision": "8676657be766e0ddd3f45b758339b816",
+    "url": "/adarsh-nellore/static/media/reverb2.8676657b.png"
+  },
+  {
+    "revision": "5b3ed930d4eabe649ada1855261197b9",
+    "url": "/adarsh-nellore/static/media/roommodes.5b3ed930.jpg"
+  },
+  {
+    "revision": "3733d39204ee64392f1c9203ff9436af",
+    "url": "/adarsh-nellore/static/media/musicalchart.3733d392.jpg"
+  },
+  {
+    "revision": "69820c3f2184600538f3f224eea154c6",
+    "url": "/adarsh-nellore/static/media/roommodecube.69820c3f.mov"
+  },
+  {
+    "revision": "8dfac3053f65803733fe1e3f2d2022dc",
+    "url": "/adarsh-nellore/static/media/questions.8dfac305.png"
+  },
+  {
+    "revision": "a72599fb1a843810fc56ec1f7dd2566b",
+    "url": "/adarsh-nellore/static/media/Stakeholders-100.a72599fb.jpg"
+  },
+  {
+    "revision": "b0a70c18a26f4ddd109ff23a31e9e4ab",
+    "url": "/adarsh-nellore/static/media/Resonance.b0a70c18.png"
+  },
+  {
+    "revision": "457c31de53fdcfc7af9fe596db8f6f72",
+    "url": "/adarsh-nellore/static/media/SpatialDemo.457c31de.mov"
+  },
+  {
+    "revision": "549c8358ab37dd35d420d912f70ffa9e",
+    "url": "/adarsh-nellore/static/media/grandcentral.549c8358.mp4"
+  },
+  {
+    "revision": "bbb46540624403f51e1d4933ef52f746",
+    "url": "/adarsh-nellore/static/media/TitleVideo.bbb46540.mp4"
+  },
+  {
+    "revision": "9d53d0fe959bcb055f034b4b6dfcd7c5",
+    "url": "/adarsh-nellore/static/media/magicwand.9d53d0fe.mp3"
+  },
+  {
+    "revision": "bed3d8744228952ef767ba4c72dd8368",
+    "url": "/adarsh-nellore/static/media/tyler.bed3d874.jpg"
+  },
+  {
+    "revision": "af28b063332395c19d162999aa7db3f7",
+    "url": "/adarsh-nellore/static/media/radiohead.af28b063.jpg"
+  },
+  {
+    "revision": "d03e3a0b35e0bdc20efa463f00b1ebdf",
+    "url": "/adarsh-nellore/static/media/pinknoise.d03e3a0b.jpg"
+  },
+  {
+    "revision": "15155e68e1b32dbdd590b0c00791ba5e",
+    "url": "/adarsh-nellore/static/media/Binaural.15155e68.mp4"
+  },
+  {
+    "revision": "ce61ac38fa69b85d6086058b4e3cff70",
+    "url": "/adarsh-nellore/static/media/userjourney.ce61ac38.jpg"
+  },
+  {
+    "revision": "1b3a47ce29e39fd024b6c0c6e64afb99",
+    "url": "/adarsh-nellore/index.html"
+  }
+];
